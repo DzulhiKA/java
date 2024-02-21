0f0d2cb72b487b758f85e6e32b5642e98a5cbd20
dzulhika <dzulhikurniawanadha@gmail.com>
2024-02-21 22:39:16 +0700

    codelab dzulhi kurniawan adha

--- src/codelab.java
+++ src/codelab.java
@@ -1,4 +1,6 @@
 import  java.util.Scanner;
+import java.time.LocalDate;
+import java.time.Period;
 public class codelab {
 
     public static void main(String[] args) {
@@ -14,17 +16,23 @@ public class codelab {
         System.out.print("Masukkan Jenis Kelamin (P/L)    :");
         jenisKelamin = input.next().charAt(0);
         input.nextLine();
-        System.out.print("Tanggal Lahir (DD/MM/YYYY)       :");
-        tanggalLahir = input.nextLine();
+        System.out.print("Tanggal Lahir (YYYY-MM-DD)       :");
+        tanggalLahir = input.next();
+
+        LocalDate lahir = LocalDate.parse(tanggalLahir);
+        LocalDate hariIni = LocalDate.now();
+        Period periode = Period.between(lahir, hariIni);
+        int umur = periode.getYears();
+        int bulan = periode.getMonths();
 
         if (jenisKelamin == 'P' || jenisKelamin == 'p'){
             System.out.println("Nama            :"+nama);
             System.out.println("Jenis Kelamin   : Perempuan");
-            System.out.println("Tanggal Lahir   :"+tanggalLahir);
+            System.out.printf("Umur             :"+ umur +" Tahun"+ bulan +" bulan");
         } else if (jenisKelamin == 'L' || jenisKelamin == 'l') {
             System.out.println("Nama            :"+nama);
             System.out.println("Jenis Kelamin   : Laki laki");
-            System.out.println("Tanggal Lahir   :"+tanggalLahir);
+            System.out.printf("Umur             :"+ umur +" Tahun "+ bulan +" bulan");
         }else {
             System.out.println("Input Jenis Kelamin tidak valid");
         }
